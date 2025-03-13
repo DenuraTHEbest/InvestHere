768a5dd394ec7a6ba7e6a79399a7198ed3bff90a
Vishwajith50 <kavishanvishwajith@gmail.com>
2025-03-13 09:14:53 +0530

    front end integration

--- FrontEnd/.next/server/webpack-runtime.js
+++ FrontEnd/.next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("90dfdc2ffc761320")
+/******/ 		__webpack_require__.h = () => ("4086ee1776b97cda")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
